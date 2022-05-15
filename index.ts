f401d9f6c90d5d6e3313bcebbf937d561fe82aac
Roni Frantchi <roni-frantchi@users.noreply.github.com>
2022-05-15 15:33:49 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,20 +1,7 @@
-// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
-import * as awsx from "@pulumi/awsx";
 import * as eks from "@pulumi/eks";
 
-// Create a VPC for our cluster.
-const vpc = new awsx.ec2.Vpc("vpc", { numberOfAvailabilityZones: 2 });
-
-// Create the EKS cluster itself and a deployment of the Kubernetes dashboard.
-const cluster = new eks.Cluster("cluster", {
-    vpcId: vpc.id,
-    subnetIds: vpc.publicSubnetIds,
-    instanceType: "t2.medium",
-    desiredCapacity: 2,
-    minSize: 1,
-    maxSize: 2,
-});
+// Create an EKS cluster with the default configuration.
+const cluster = new eks.Cluster("k8s-template-cluster");
 
 // Export the cluster's kubeconfig.
 export const kubeconfig = cluster.kubeconfig;
