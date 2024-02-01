7b60d452dddada1f513e91a9de8a95a74634c05f
ChaimP <chaim.platonov@env0.com>
2024-02-01 11:41:48 +0200

    add tags

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import * as eks from "@pulumi/eks";
 
 // Create an EKS cluster with the default configuration.
-const cluster = new eks.Cluster("k8s-template-integration-tests");
+const cluster = new eks.Cluster("k8s-templates-integration-tests", {
+    clusterTags: {
+        owner: "RND",
+        DO_NOT_DELETE: "DO_NOT_DELETE",
+    }});
 
 export const clusterName = cluster.eksCluster.name;
